38b0c67ca4d08df6b379f5493176991ac222627f
Offor Obinna Michael <mchlobinna95@gmail.com>
2024-01-25 13:21:43 +0100

    key pairs

---
+++ vars.tf
@@ -0,0 +1,97 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-0c7217cdde317cfec"
+    us-east-2 = "ami-05fb0b8c1424f266b"
+    us-west-1 = "ami-0ce2cb35386fc22e9"
+  }
+}
+
+variable "PRIV_KEY_PATH" {
+  default = "vprofilekey"
+}
+
+variable "PUB_KEY_PATH" {
+  default = "vprofilekey.pub"
+}
+
+variable "USERNAME" {
+  default = "Ubuntu"
+}
+
+variable "MYIP" {
+  default = "102.89.43.203/32"
+}
+
+variable "rmquser" {
+  default = "rabbit"
+}
+
+variable "rmqpass" {
+  default = "Offor83@_116202"
+}
+
+variable "dbuser" {
+  default = "admin"
+}
+
+variable "dbpass" {
+  default = "admin123"
+}
+
+variable "dbname" {
+  default = "accounts"
+}
+
+variable "instance_count" {
+  default = "1"
+}
+
+variable "VPC_NAME" {
+  default = "vprofile-VPC"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "ZONE2" {
+  default = "us-east-1b"
+}
+
+variable "ZONE3" {
+  default = "us-east-1c"
+}
+
+variable "VpcCIDR" {
+  default = "172.21.0.0/16"
+}
+
+variable "PubSub1CIDR" {
+  default = "172.21.1.0/24"
+}
+
+variable "PubSub2CIDR" {
+  default = "172.21.2.0/24"
+}
+
+variable "PubSub3CIDR" {
+  default = "172.21.3.0/24"
+}
+
+variable "PrivSub1CIDR" {
+  default = "172.21.4.0/24"
+}
+
+variable "PrivSub2CIDR" {
+  default = "172.21.5.0/24"
+}
+
+variable "PrivSub3CIDR" {
+  default = "172.21.6.0/24"
+}
+
